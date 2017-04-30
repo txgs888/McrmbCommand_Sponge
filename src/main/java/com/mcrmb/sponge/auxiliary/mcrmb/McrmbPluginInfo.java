a431c112f324946d09bc041b9afc4ea3e6dc98d2
txgs888 <txgs888@gmail.com>
2017-04-30 14:36:54 +0800

    计划使用json来读取和配置可购买物品列表

--- src/main/java/com/mcrmb/sponge/auxiliary/mcrmb/McrmbPluginInfo.java
+++ src/main/java/com/mcrmb/sponge/auxiliary/mcrmb/McrmbPluginInfo.java
@@ -1,13 +1,5 @@
 package com.mcrmb.sponge.auxiliary.mcrmb;
 
-import com.google.common.reflect.TypeToken;
-import ninja.leaping.configurate.commented.CommentedConfigurationNode;
-import ninja.leaping.configurate.objectmapping.ObjectMappingException;
-
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by txgs888 on 2017/4/12.
  */
@@ -16,35 +8,4 @@ public class McrmbPluginInfo {
     public final static String NAME = "McrmbCommand";
     public final static String VERSION = "1.0.0";
     public final static String AUTHORS = "mcrmb.com";
-
-    public static CommentedConfigurationNode commentedConfig;
-
-    public static class config {
-
-    }
-
-
-    public static void initMcrmbCore() {
-        commentedConfig = ConfigManager.get().getConfig();
-    }
-
-    public static void save() {
-        try {
-            //使用反射保存config类中的所有变量
-            Class<?> configClass = Class.forName("com.mcrmb.sponge.auxiliary.mcrmb.McrmbPluginInfo$config");
-            Field[] fields = configClass.getDeclaredFields();
-            CommentedConfigurationNode node = ConfigManager.get().getConfig();
-            for (Field field : fields) {
-                try {
-                    node.getNode(field.getName()).setValue(field.get(configClass));
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-            ConfigManager.get().save();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
